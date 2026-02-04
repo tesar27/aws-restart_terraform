aac96abca49f07772552d350e49d2662fa6389d5
tesar27 <tesar.public@gmail.com>
2026-02-04 15:25:09 +0100

    ec2 dynamic fetch

--- compute.tf
+++ compute.tf
@@ -1,5 +1,15 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
 resource "aws_instance" "cli_host" {
-  ami           = "ami-0c55b159cbfafe1d0"  # Amazon Linux 2 in us-west-2; update if needed
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
