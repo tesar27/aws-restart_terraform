479a522b5a7ad1a387a0883bd294f330cdf94ea0
tesar27 <tesar.public@gmail.com>
2026-02-18 09:58:36 +0100

    renaming resources and attributes due to deprecation

--- storage.tf
+++ storage.tf
@@ -1,13 +1,13 @@
 resource "aws_s3_bucket" "files" {
   bucket_prefix = "aws-capstone-files-"
-  acl           = "private"
+  aws_s3_bucket_acl           = "private"
   force_destroy = true # convenient for dev (removes objects on destroy)
 
-  versioning {
+  aws_s3_bucket_versioning {
     enabled = true
   }
 
-  server_side_encryption_configuration {
+  aws_s3_bucket_server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
         sse_algorithm = "AES256"
@@ -32,7 +32,7 @@ resource "aws_s3_bucket_public_access_block" "files_block" {
 resource "aws_dynamodb_table" "files" {
   name         = "${terraform.workspace}-Files"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "userId"
+  key_schema     = "userId"
   range_key    = "fileId"
 
   attribute {
@@ -46,7 +46,7 @@ resource "aws_dynamodb_table" "files" {
 
   global_secondary_index {
     name            = "fileId-index"
-    hash_key        = "fileId"
+    key_schema        = "fileId"
     projection_type = "ALL"
   }
 
@@ -64,7 +64,7 @@ resource "aws_dynamodb_table" "files" {
 resource "aws_dynamodb_table" "users" {
   name         = "${terraform.workspace}-Users"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "userId"
+  key_schema     = "userId"
 
   attribute {
     name = "userId"
@@ -78,7 +78,7 @@ resource "aws_dynamodb_table" "users" {
 
   global_secondary_index {
     name            = "email-index"
-    hash_key        = "email"
+    key_schema        = "email"
     projection_type = "ALL"
   }
 
